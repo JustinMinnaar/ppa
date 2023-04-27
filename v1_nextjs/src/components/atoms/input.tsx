62a40eb70d3d95cf95c8daa0e28a94fcf7f23e71
Justin Minnaar <Justin@Minnaar.org>
2023-04-27 14:05:00 +0200

    check box atom

--- v1_nextjs/src/components/atoms/input.tsx
+++ v1_nextjs/src/components/atoms/input.tsx
@@ -1,19 +1,40 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
+import { Checkbox } from '@mui/material';
 
-interface props {
-    id: string;
-    label: string;
+interface propsTextInput {
+    id?: string;
+    label?: string;
     variant: 'filled' | 'outlined' | 'standard';
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
     value: string;
-    className: any;
-    placeholder: string
+    className?: any;
+    placeholder?: string,
+    type?:string
 }
 
 
-const BasicTextFields: React.FC<props> = ({ id, label, variant, onChange, value, className, placeholder }) => {
+const BasicTextFields: React.FC<propsTextInput> = ({ type, id, label, variant, onChange, value, className, placeholder }) => {
     return (
-        <TextField placeholder={placeholder} id={id} label={label} variant={variant} sx={className} />
+        <TextField placeholder={placeholder} id={id} type={type} label={label} variant={variant} sx={className} onChange={onChange} />
+    );
+}
+
+interface propsCheckbox {
+    defaultChecked?: boolean;
+    checked: boolean;
+    disabled?: boolean;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    value?: any;
+    id?: string;
+    label?: string;
+}
+
+const Checkboxes: React.FC<propsCheckbox> = ({ defaultChecked, checked, onChange, disabled, value, id, label }) => {
+    return (
+        <div style={{ display: 'flex', flexDirection:'row'}}>
+            <Checkbox checked={checked} onChange={onChange} defaultChecked={defaultChecked} disabled={disabled} value={value} id={id} />
+            <label>{label}</label>
+        </div>
     );
 }
